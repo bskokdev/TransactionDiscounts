e103d8dec442a57b985b481d6c01b9f167c4cdf7
Boris Skok <skokcmd@gmail.com>
2023-04-13 22:47:49 +0200

    added definitions to the application header & removed transactions repo

--- Application.h
+++ Application.h
@@ -3,12 +3,11 @@
 
 
 #include "shipping/shippingOption/ShippingOptionRepository.h"
-#include "transaction/TransactionPrinter.h"
 
 #include <string>
 #include <iostream>
 #include "io/Reader.h"
-#include "transaction/TransactionRepository.h"
+#include "transaction/Transaction.h"
 
 /**
  * @brief Class representing the application
@@ -16,14 +15,47 @@
 class Application {
 private:
     ShippingOptionRepository& shippingRepo;
-    TransactionRepository& transactionRepo;
+
+    /**
+     * @brief Read input file path from user
+     * @return input file path
+     */
+    std::string readInputFilePath();
+
+    /**
+     * @brief Read input file to vector of strings
+     * @param inputFilePath file path
+     * @return vector of lines
+     */
+    std::vector<std::string> readInputFile(std::string &inputFilePath);
+
+    /**
+     * @brief Create a Transaction object from an input line
+     * @param line input line
+     * @return Transaction object
+     */
+    Transaction createTransaction(std::string &line);
+
+    /**
+     * @brief Apply discount on the transaction and return the discount
+     * @param transaction Transaction object
+     * @return discount
+     */
+    double applyDiscount(Transaction &transaction);
+
+    /**
+     * @brief Print the transaction and discount
+     * @param transaction Transaction object
+     * @param discount discount
+     */
+    void printTransactionAndDiscount(Transaction &transaction, double discount);
 public:
     /**
      * @brief Constructor with dependencies
      * @details The dependencies are injected via constructor
      * @param shippingRepo ShippingOptionRepository instance
      */
-    Application(ShippingOptionRepository &shippingRepo, TransactionRepository &transactionRepo);
+    Application(ShippingOptionRepository &shippingRepo);
 
     /**
      * @brief Run the application
