bf233b793036d748a30faab1805a7e12b53c6e47
Boris Skok <skokcmd@gmail.com>
2023-04-13 22:16:14 +0200

    docs

--- common/IDataRepository.h
+++ common/IDataRepository.h
@@ -11,10 +11,35 @@ class IDataRepository {
 public:
     virtual ~IDataRepository() = default;
 
+    /**
+     * @brief Check if the repository contains the given item
+     * @param item
+     * @return true if the repository contains the given item
+     */
     virtual bool exists(const T& item) = 0;
+
+    /**
+     * @brief Add the given item to the repository
+     * @param item
+     */
     virtual void add(const T& item) = 0;
+
+    /**
+     * @brief Remove the given item from the repository
+     * @param item
+     */
     virtual void remove(const T& item) = 0;
+
+    /**
+     * @brief Update the given item in the repository
+     * @param item
+     */
+
     virtual void update(const T& item) = 0;
+    /**
+     * @brief Get all items from the repository
+     * @return std::vector<T>
+     */
     virtual std::vector<T> getAll() = 0;
 };
 
