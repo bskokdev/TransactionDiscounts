2dc28bd14155982836d30aa552212f76dc6d82df
Boris Skok <skokcmd@gmail.com>
2023-04-15 21:17:03 +0200

    docs

--- validation/IValidator.h
+++ validation/IValidator.h
@@ -2,9 +2,18 @@
 #define TRANSACTIONDISCOUNTS_IVALIDATOR_H
 
 
+/**
+ * @brief Interface for validators
+ * @tparam T type of value to validate
+ */
 template <typename T>
 class IValidator {
 public:
+    /**
+     * @brief Check if the value is valid
+     * @param value Value to validate
+     * @return true if the value is valid, false otherwise
+     */
     virtual bool isValid(T value) = 0;
 };
 
